64105c85911d3dc75d414000cedf623e4f0ecdef
Tkachuk, Adel <tkachukadel@gmail.com>
2025-02-11 13:12:38 -0500

    added some style changes

--- src/main/java/tkachuk/physics/Location.java
+++ src/main/java/tkachuk/physics/Location.java
@@ -12,30 +12,25 @@ public class Location
         this.y = y;
     }
 
-
     public void setX(double x)
     {
         this.x = x;
     }
-
 
     public double getX()
     {
         return x;
     }
 
-
     public void setY(double y)
     {
         this.y = y;
     }
 
-
     public double getY()
     {
         return y;
     }
-
 
     public Location apply(Force f)
     {
